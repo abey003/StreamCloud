48dff061d23e59ae3fb47d37d2a09e0fecfb0c09
abey003 <abeys2003@gmail.com>
2024-08-28 14:53:18 +0530

    Optimize commit

--- streamcloud-frontend/src/components/BrowseMoviesPage.jsx
+++ streamcloud-frontend/src/components/BrowseMoviesPage.jsx
@@ -13,7 +13,7 @@ const BrowseMoviesPage = () => {
 
   useEffect(() => {
     setLoading(true); // Start loading when API call is made
-    axios.get(`http://localhost:3000/movies/genre/${genre}`)
+    axios.get(`https://streamcloud-lt16.onrender.com/movies/genre/${genre}`)
       .then((res) => {
         setMovies(res.data);
         setLoading(false); // Stop loading when data is fetched
